7480b732bae37d89414061773c93e223de36361b
OL-EPIC <oleksiilapin@gmail.com>
2026-05-04 09:02:39 +0300

    test

---
+++ pelisivu/js/main.js
@@ -0,0 +1,8 @@
+const playgame = document.getElementById("game")
+const playgamehome = document.getElementById("gamehome")
+playgame.addEventListener("click", () => {
+    console.log("yes")
+})
+playgamehome.addEventListener("click", () => {
+    console.log("yes")
+})
